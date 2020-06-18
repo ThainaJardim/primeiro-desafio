25fceec149aa9502a3f95c5fc48a74598d59a056
ThainaDev <thainaj.aguiar@gmail.com>
2020-06-17 21:44:56 -0300

    Primeiro commit

---
+++ desafio1.js
@@ -0,0 +1,8 @@
+const nome='Jorge'
+const peso=77
+const altura=1.77
+const imc= peso/ (altura*altura)
+if (imc >= 30)  {
+    console.log ('Jorge você está acima do peso')
+} else (imc<29.9)
+    console.log ('Jorge você não está acima do peso')
